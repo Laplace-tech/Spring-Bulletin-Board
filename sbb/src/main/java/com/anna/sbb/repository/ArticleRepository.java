75f939ce3f02e5fe2c6ef15407d82a36c4e0480e
Laplace <add28482848@gmail.com>
2024-11-02 20:27:08 +0900

    Delete sbb directory

--- sbb/src/main/java/com/anna/sbb/repository/ArticleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anna.sbb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anna.sbb.domain.Article;
-
-public interface ArticleRepository extends JpaRepository<Article, Long>{
-
-}
