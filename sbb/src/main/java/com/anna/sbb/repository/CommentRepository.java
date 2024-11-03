8326c58748493e3609a875e5222072445f043461
Laplace <add28482848@gmail.com>
2024-11-04 00:15:52 +0900

    Delete sbb directory

--- sbb/src/main/java/com/anna/sbb/repository/CommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anna.sbb.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.anna.sbb.domain.Comment;
-
-public interface CommentRepository extends JpaRepository<Comment, Long>{
-
-}
